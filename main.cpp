eb21cca376c412177387289186cb07890c3ea747
amin <aminklhr@gmail.com>
2023-05-13 17:08:43 +0330

    Amin1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int-long-long print(int-long-long number)
+{
+    int-long-long result = number * number ;
+    return(result) ;
+
+}
+
+int main()
+{
+    bool flaq = true ;
+    while(flaq)
+    {
+    int-long-long number1 ;
+    cin >> number1 ;
+    cout << print(number1) << "\n" ;}
+    return 0 ;
+
+}
